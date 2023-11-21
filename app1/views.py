f7ae08a5068948256be217581b16ce456e01d909
JyothiSiddamReddy <jyothisiddamreddy2002@gmail.com>
2023-11-21 19:13:05 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def jyothi(request):
+    return render(request,'jyothi.html')
+
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def kiran(request):
+    return render(request,'kiran.html')
